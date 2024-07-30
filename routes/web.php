e89de7d740e9c489fac99baed16a86a2f8dbbec4
Flávio Santos <flavioanalistadesistema@gmail.com>
2024-07-30 12:12:20 -0300

    feat: Include router apiCep

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
-
+use App\Http\Controllers\CepController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/cep/{cep}', [CepController::class, 'consultar']);
